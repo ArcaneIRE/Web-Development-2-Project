016e11f5b7a1fc9c9c2b92a9c1243d07a83d2de4
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-05 12:02:00 +0000

    Create basic footer

--- view/include/footer.php
+++ view/include/footer.php
@@ -1,4 +1,9 @@
 <link rel="stylesheet" href="./view/css/include/footer.css" type="text/css">
 <footer>
-    Footer
+    <div class="footer-column">
+        <p>CMPU2022 Web Development 2</p>
+    </div>
+    <div class="footer-column">
+        Developed by <a href="https://www.davidcorcoran.ie/" class="personal-site">David Corcoran</a>
+    </div>
 </footer>
