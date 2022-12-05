57de737f9f390ff3812b2770359416488f3763ce
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-05 12:07:53 +0000

    Fix footer column alignment

--- view/include/footer.php
+++ view/include/footer.php
@@ -4,6 +4,6 @@
         <p>CMPU2022 Web Development 2</p>
     </div>
     <div class="footer-column">
-        Developed by <a href="https://www.davidcorcoran.ie/" class="personal-site">David Corcoran</a>
+        <p>Developed by <a href="https://www.davidcorcoran.ie/" class="personal-site">David Corcoran</a></p>
     </div>
 </footer>
