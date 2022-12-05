98fcac33e8ee1c49d2e2c96aecd2f422d9fd61fd
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-05 12:02:30 +0000

    Create basic header

--- view/include/header.php
+++ view/include/header.php
@@ -1,4 +1,11 @@
 <link rel="stylesheet" href="./view/css/include/header.css" type="text/css">
 <header>
-    <h1>Local Library</h1>
+    <h1>
+        <a href="index.php">📚 Local Library</a>
+    </h1>
+
+    <nav class="header-nav">
+        <a href="search.php">Search</a>
+        <a href="account.php">My Account</a>
+    </nav>
 </header>
