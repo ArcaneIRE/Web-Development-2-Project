31c72b22b58430e1e8203eebe11aa84832effe78
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-09 04:58:23 +0000

    Add registration database connection

--- view/register.php
+++ view/register.php
@@ -55,10 +55,79 @@
       </div>
       <div class="row">
         <label for="mobile">Mobile: <strong class='form-requirement'>(10 chars)</strong></label>
-        <input type="text" name="mobile" id="mobile">
+        <input type="text" name="mobile" id="mobile" minlength="10" maxlength="10">
       </div>
       <button class="submit">Submit</button>
     </form>
+    <?php 
+      if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['confirmPassword']) && isset($_POST['firstName'])) {
+        require_once $root . '/model/database_connect.php';
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $confirmPassword = $_POST['confirmPassword'];
+        $firstName = $_POST['firstName'];
+        $surname = '';
+        if (isset($_POST['Surname'])) {
+          $surname = $_POST['Surname'];
+        }
+        $addressLineOne = '';
+        if (isset($_POST['addressLineOne'])) {
+          $addressLineOne = $_POST['addressLineOne'];
+        }
+        $addressLineTwo = '';
+        if (isset($_POST['addressLineTwo'])) {
+          $addressLineTwo = $_POST['addressLineTwo'];
+        }
+        $city = '';
+        if (isset($_POST['city'])) {
+          $city = $_POST['city'];
+        }
+        $telephone = '';
+        if (isset($_POST['telephone'])) {
+          $telephone = $_POST['telephone'];
+        }
+        $mobile = '';
+        if (isset($_POST['mobile'])) {
+          $mobile = $_POST['mobile'];
+        }
+
+        // Input Validation
+        $usernameQuery = "SELECT username FROM users WHERE username = '$username'";
+        $result = $conn->query($usernameQuery);
+        if ($result->num_rows > 0) {
+          echo '<p class="error">Error! That username is taken.</p>';
+          return;
+        } 
+        elseif ($_POST['password'] !== $_POST['confirmPassword']) {
+          echo '<p class="error">Error! Passwords did not match.</p>';
+          return;
+        }
+        elseif (strlen($password) !== 6) {
+          echo '<p class="error">Error! Passwords must be 6 characters long.</p>';
+          return;
+        }
+        elseif (isset($_POST['mobile'])) {
+          if (!is_numeric($_POST['mobile'])) {
+            echo '<p class="error">Error! Mobile numbers may only contain numbers.</p>';
+            return;
+          }
+          elseif (strlen($_POST['mobile']) !== 10) {
+            echo '<p class="error">Error! Mobile numbers must be 10 digits long.</p>';
+            return;
+          }
+        }
+        // Success path
+        $newUserInsert = "INSERT INTO users VALUES ('$username', '$password', '$firstName', '$surname', '$addressLineOne', '$addressLineTwo', '$city', '$telephone', '$mobile')";
+        $result = $conn->query($newUserInsert);
+        if ($result === true) {
+          echo '<p class="success">Success! New Account Created!</p>';
+        } else {
+          echo  '<p class="error">Database Error: New user insert failed. ' . $conn->error . '</p>';
+        }
+        $conn->close();
+      }
+    ?>
   </main>
 
   <?php
