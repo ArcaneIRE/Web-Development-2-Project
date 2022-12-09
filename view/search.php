73cde158e45fabf525ea516922265ccfa682db6b
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-09 09:17:57 +0000

    Add search

---
+++ view/search.php
@@ -0,0 +1,139 @@
+<?php 
+    session_start();
+    if (!isset($_SESSION['username'])) {
+        header('Location: index.php');
+        return;
+    }
+
+    $root = $_SERVER['DOCUMENT_ROOT'] . '/web-dev-project';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/web-dev-project/view/css/search.css">
+    <title>Search</title>
+</head>
+<body>
+  <?php
+      include $root . '/view/include/header.php';
+  ?>
+
+  <main>
+      <h2>Search</h2>
+      <div class="search-forms">
+        <form method="post" class="search-form">
+          <?php
+            $value = 'placeholder="Type here"';
+            if (isset($_POST['search-term'])) {
+              $value = 'value="' . $_POST['search-term'] . '"';
+            }
+            echo '<input type="text" id="search-term" name="search-term" ' . $value . ' required>'
+          ?>
+          <button>Search</button>
+        </form>
+        <form method="post" class="category-form">
+          <select id="category-id" name="category-id" placeholder="Category" required>
+            <option value="" disabled selected hidden>Select a Category</option>
+            <?php
+              require_once $root . '/model/database_connect.php';
+              $categorySelectQuery = 'SELECT id, title FROM categories';
+              $result = $conn->query($categorySelectQuery);
+              while ( $row = $result->fetch_assoc()) {
+                echo '<option value="' . $row['id'] . '">' . $row['title'] . '</option>';
+              }
+            ?>
+          </select>
+          <button>Search</button>
+        </form>
+      </div>
+      <?php
+        require_once $root . '/model/database_connect.php';
+        $result = 0;
+        if (isset($_POST['search-term'])) {
+          $search_term = $_POST['search-term'];
+          $titleSearch = "SELECT * FROM books WHERE title LIKE '%$search_term%'";
+          $authorSearch = "SELECT * FROM books WHERE author LIKE '%$search_term%'";
+          $searchQuery = $titleSearch . ' UNION ' . $authorSearch;
+          $result = $conn->query($searchQuery);
+        }
+        if (isset($_POST['category-id'])) {
+          $category_id = $_POST['category-id'];
+          $categoryQuery = "SELECT * FROM books WHERE category_id = '$category_id'";
+          $result = $conn->query($categoryQuery);
+        }
+
+        if (!isset($result->num_rows)) {
+          echo '<p>No Results</p>';
+        }
+        elseif ($result->num_rows === 0) {
+          echo '<p>No Results</p>';
+        }
+        elseif ($result->num_rows > 0) {
+          // Table Titles
+          echo '<table class="results">';
+          echo '<tr>';
+          echo '<th>ISBN</th>';
+          echo '<th>Title</th>';
+          echo '<th>Author</th>';
+          echo '<th>Edition</th>';
+          echo '<th>Year</th>';
+          echo '<th>Category</th>';
+          echo '<th>Reserved</th>';
+          echo '<th>Make Reservation</th>';
+          echo '</tr>';
+
+          $offset = 0;
+          if (isset($_POST['offset'])) {
+            $offset = $_POST['offset'];
+          }
+
+          $rows = $result->fetch_all();
+          $current_index = $offset;
+
+          while((array_key_exists($current_index, $rows)) && ( $current_index < $offset + 5) ) {
+            $row = $rows[$current_index];
+            $current_index = $current_index + 1;
+            
+            echo '<tr>';
+            echo '<td>' . $row[0] . '</td>';
+            echo '<td>' . $row[1] . '</td>';
+            echo '<td>' . $row[2] . '</td>';
+            echo '<td>' . $row[3] . '</td>';
+            echo '<td>' . $row[4] . '</td>';
+            echo '<td>' . $row[5] . '</td>';
+            if ($row[6]) {
+              echo '<td>Yes</td>';
+            } else {
+              echo '<td>No</td>';
+            }
+            echo '<td>Reserve</td>';
+            echo '</tr>';
+          }
+          echo '</table>';
+
+          if ( $result->num_rows > 5 ) {
+            echo '<form method="post" class="page-buttons">';
+            if (isset($_POST['search-term'])) {
+              echo '<input type="hidden" name="search-term" value="' . $_POST['search-term'] . '"/>';
+            } elseif (isset($_POST['category-id'])) {
+              echo '<input type="hidden" name="category-id" value="' . $_POST['category-id'] . '"/>';
+            }
+            if (($offset - 5) >= 0 ) {
+                echo '<button type="submit" name="offset" value="'. ($offset - 5) . '">Previous</button>';
+            }
+            if ( $result->num_rows > ($offset + 5) ) {
+                echo '<button type="submit" name="offset" value="' . ($offset + 5) . '">Next</button>';
+            }
+            echo '</form>';
+          }
+        }
+      ?>
+  </main>
+
+  <?php
+      include $root . '/view/include/footer.php';
+  ?>
+</body>
+</html>
