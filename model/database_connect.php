c4bf49127d93b620dbae3688eb16b1ed6fb3550d
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-09 09:56:29 +0000

    Change DB

--- model/database_connect.php
+++ model/database_connect.php
@@ -2,7 +2,7 @@
   $servername = 'localhost';
   $username = 'root';
   $password = '';
-  $database = 'testdb';
+  $database = 'projectdb';
 
   $conn = new mysqli($servername, $username, $password, $database);
 
