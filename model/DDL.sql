e1545189f3469c45d14d2ac981f2e217ea8774bc
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-09 03:39:00 +0000

    Fix mobile datatype to spec

--- model/DDL.sql
+++ model/DDL.sql
@@ -6,14 +6,14 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE
     users (
         username VARCHAR(30) NOT NULL PRIMARY KEY,
-        password VARCHAR(30) NOT NULL,
+        password VARCHAR(6) NOT NULL,
         firstname VARCHAR(30) NOT NULL,
         surname VARCHAR(30),
         AddressLine1 VARCHAR(50) NOT NULL,
         AddressLine2 VARCHAR(50),
         city VARCHAR(30),
         telephone VARCHAR(15),
-        mobile VARCHAR(15)
+        mobile VARCHAR(10)
     );
 
 CREATE TABLE
