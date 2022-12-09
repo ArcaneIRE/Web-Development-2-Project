fac1642c817b238298cf3d4667dedcead2a6f3ca
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-09 08:44:02 +0000

    Simplify pagination code

--- view/reservations.php
+++ view/reservations.php
@@ -54,30 +54,23 @@
                     echo '</table>';
                     echo '<p>No Reservations Currently Made</p>';
                 } else {
-                    $offset = 0;
                     // Next and Previous Buttons
-                    if (isset($_POST['page'])) {
-                        if ($_POST['page'] === 'next') {
-                            $offset = $offset + 5;
-                        } else {
-                            $offset = $offset - 5;
-                            if ($offset < 0) {
-                                $offset = 0;
-                            }
-                        }
+                    $offset = 0;
+                    if (isset($_POST['offset'])) {
+                      $offset = $_POST['offset'];
                     }
+
                     $rows = $result->fetch_all();
-                    $displayed_count = 0;
                     $current_index = $offset;
-                    while((array_key_exists($offset + $displayed_count, $rows)) && ( $displayed_count < 5) ) {
-                        $current_index = $offset + $displayed_count;
+
+                    while((array_key_exists($current_index, $rows)) && ( $current_index < $offset + 5) ) {
                         $row = $rows[$current_index];
-                        $displayed_count = $displayed_count + 1;
+                        $current_index = $current_index + 1;
 
                         echo '<tr>';
-                        echo '<td>' . $row['0'] . '</td>';
-                        echo '<td>' . $row['1'] . '</td>';
-                        echo '<td>' . $row['2'] . '</td>';
+                        echo '<td>' . $row[0] . '</td>';
+                        echo '<td>' . $row[1] . '</td>';
+                        echo '<td>' . $row[2] . '</td>';
                         // Cancel Button
                         echo '<td class="cancel-column">';
                         echo '<form method="post">';
@@ -92,10 +85,10 @@
                     if ( $result->num_rows > 5 ) {
                         echo '<form method="post" class="page-buttons">';
                         if (($offset - 5) >= 0 ) {
-                            echo '<button type="submit" name="page" value="previous">Previous</button>';
+                            echo '<button type="submit" name="offset" value="'. ($offset - 5) . '">Previous</button>';
                         }
                         if ( $result->num_rows > ($offset + 5) ) {
-                            echo '<button type="submit" name="page" value="next">Next</button>';
+                            echo '<button type="submit" name="offset" value="' . ($offset + 5) . '">Next</button>';
                         }
                         echo '</form>';
                     }
