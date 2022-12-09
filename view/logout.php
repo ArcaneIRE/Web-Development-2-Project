c2981b903bd5801eac83e124f0288c3b7b676466
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-12-09 05:40:12 +0000

    Create logout page

---
+++ view/logout.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    session_destroy();
+    header('Location: /web-dev-project/index.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="default_styles.css">
+    <title>Log Out</title>
+</head>
+<body>
+    
+</body>
+</html>
